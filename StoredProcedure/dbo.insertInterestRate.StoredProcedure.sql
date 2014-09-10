727593a778626194e1c959bcfdea19183ff99e46
Levin Calado <levincalado@gmail.com>
2014-09-10 17:36:12 +0800

    Removed Per Field

--- StoredProcedure/dbo.insertInterestRate.StoredProcedure.sql
+++ StoredProcedure/dbo.insertInterestRate.StoredProcedure.sql
@@ -1,20 +1,22 @@
 USE [CMS]
 GO
 
-/****** Object:  StoredProcedure [dbo].[insertInterestRate]    Script Date: 09/07/2014 11:32:05 ******/
+/****** Object:  StoredProcedure [dbo].[insertInterestRate]    Script Date: 09/10/2014 17:27:19 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
+
 CREATE PROCEDURE [dbo].[insertInterestRate]
-		(@TypeId AS INT, @InterestRate AS DECIMAL(7,4), @Per AS VARCHAR(50), @MinRange AS DECIMAL(18, 2))
+		(@TypeId AS INT, @InterestRate AS DECIMAL(7,4), @MinRange AS DECIMAL(18, 2))
 AS
 BEGIN
-	INSERT INTO INTEREST_RATE(AccountTypeId, InterestRate, Per, MinimumRange)
-	VALUES (@TypeId, @InterestRate, @Per, @MinRange);
+	INSERT INTO INTEREST_RATE(AccountTypeId, InterestRate, MinimumRange)
+	VALUES (@TypeId, @InterestRate, @MinRange);
 END
+
 
 
 
--- StoredProcedure/dbo.updateInterestRate.StoredProcedure.sql
+++ StoredProcedure/dbo.updateInterestRate.StoredProcedure.sql
@@ -1,19 +1,21 @@
 USE [CMS]
 GO
 
-/****** Object:  StoredProcedure [dbo].[updateInterestRate]    Script Date: 09/07/2014 11:33:09 ******/
+/****** Object:  StoredProcedure [dbo].[updateInterestRate]    Script Date: 09/10/2014 17:27:06 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
+
 CREATE PROCEDURE [dbo].[updateInterestRate]
-		(@TypeId AS INT, @InterestRate AS DECIMAL(7,4), @Per AS VARCHAR(50), @MinRange AS DECIMAL(18, 2))
+		(@TypeId AS INT, @InterestRate AS DECIMAL(7,4), @MinRange AS DECIMAL(18, 2))
 AS
 BEGIN
-	UPDATE INTEREST_RATE SET InterestRate = @InterestRate, Per = @Per, MinimumRange = @MinRange WHERE AccountTypeId = @TypeId;
+	UPDATE INTEREST_RATE SET InterestRate = @InterestRate, MinimumRange = @MinRange WHERE AccountTypeId = @TypeId;
 END
+
 
 
 
